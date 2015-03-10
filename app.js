7cfd9aca543dd9e82f3e5e8098a818ced91c13be
Uldis Sturms <uldis.sturms@gmail.com>
2015-03-10 11:37:08 +0000

    Revert "Added initall logic for insurance bot replies."
    This reverts commit 05bbd5bfba78f09f428960f70f902d618e2daeaa.

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var engine = require('./app/engine');
 var app = express();
 
 app.use(express.static('www'));
 app.use(bodyParser());
 
 app.post('/messages', function (req, res) {
-    var result = engine.process(req.body);
-    res.send(result);
+    console.log(req.body);
+    res.send({text: 'ok, got it!'});
 });
 
 module.exports = app;
